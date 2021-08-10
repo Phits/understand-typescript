5217f4b95cb62da497bf6492903d7cbceee306df
John Fitzpatrick <john.fitzpatrick@sendflex.com>
2021-08-10 14:08:22 -0400

    Class section finished

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,14 +1,17 @@
-class Department {
+abstract class Department {
+    static fiscalYear = 2021;
     // private name: string;
     protected employees: string[] = [];
 
-    constructor(private readonly id: string, public name: string) {
+    protected constructor(protected readonly id: string, public name: string) {
         // this.name = n;
     }
 
-    describe(this: Department) {
-        console.log(`Department (${this.id})`);
+    static createEmployee(name: string) {
+        return {name: name};
     }
+
+    abstract describe(this: Department): void;
 
     addEmployee(employee: string) {
         this.employees.push(employee);
@@ -27,12 +30,44 @@ class ITDepartment extends Department {
         super(id, 'IT');
         this.admins = admins;
     }
+
+    describe() {
+        console.log('IT Department ' + this.id);
+    }
 }
 
+// Accounting Department
 class AccountingDepartment extends Department {
-    constructor(id: string, private readonly reports: string[]) {
+    private lastReport: string;
+    private static instance: AccountingDepartment;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("no report found!");
+    }
+
+    set mostRecentReport(value: string) {
+        this.addReport(value);
+    }
+
+   private constructor(id: string, private readonly reports: string[]) {
         super(id, 'Accounting');
         this.reports = reports;
+        this.lastReport = reports[0];
+    }
+
+    static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('d2', []);
+        return this.instance;
+    }
+
+    describe() {
+        console.log('Accounting Department - ID ', this.id)
     }
 
     addEmployee(name: string) {
@@ -44,6 +79,7 @@ class AccountingDepartment extends Department {
 
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text[0];
     }
 
     printReports() {
@@ -52,6 +88,9 @@ class AccountingDepartment extends Department {
 }
 
 const it = new ITDepartment('d1', ['Fitz']);
+
+const employee1 = Department.createEmployee('Phil');
+console.log(employee1, Department.fiscalYear);
 
 it.addEmployee('Fitz');
 it.addEmployee('Beda');
@@ -63,7 +102,15 @@ it.printEmployeeInformation();
 
 console.log(it);
 
-const accounting = new AccountingDepartment('d2', []);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+
+console.log(accounting);
+console.log(accounting2);
+
+accounting.mostRecentReport = 'Year End Report';
+
+console.log(accounting.mostRecentReport);
 
 accounting.addReport('Something went wrong...');
 accounting.addEmployee('Fitz');
@@ -72,6 +119,8 @@ accounting.addEmployee('Danny');
 accounting.printReports();
 
 console.log(accounting);
+
+accounting.describe();
 
 // const accountingCopy = { name: 'Dummy', describe: accounting.describe};
 //
--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -1,8 +1,8 @@
-const userName = 'Fitz';
-
-let age = 30;
-
-age = 29;
+// const userName = 'Fitz';
+//
+// let age = 30;
+//
+// age = 29;
 
 // function add(a: number, b: number) {
 //     let result = a + b;
@@ -12,9 +12,61 @@ age = 29;
 // console.log(result);
 
 // Arrow Function
-const add = (a: number, b: number) =>  a + b;
+// const add = (a: number, b: number = 1) =>  a + b;
 
 const printOutput: (a: string | number) => void = output =>  console.log(output);
 
+const button = document.querySelector('button');
 
-printOutput(add(5, 3));
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
+
+// printOutput(add(5));
+
+const hobbies = ['Gardening', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
+const person = {
+    firstName: 'Fitz',
+    age: 33
+}
+
+const copiedPerson = { ...person };
+
+// Rest Parameters
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0)
+};
+
+const addNumbers = add(1, 2, 3, 4, 5);
+console.log(addNumbers);
+
+// Destructuring Arrays
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+// Destructuring Objects
+const { firstName: userName, age } = person;
+
+console.log(userName, age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
