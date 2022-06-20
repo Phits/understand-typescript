59c31f379ba04d110bbb0dc6348be7e6743d1864
John Fitzpatrick <john.fitzpatrick@sendflex.com>
2022-06-20 13:35:26 -0400

    Fixed type validation errors

--- section-12-3rd-party-libraries/src/app.ts
+++ section-12-3rd-party-libraries/src/app.ts
@@ -1,3 +1,16 @@
-// Code goes here!
+import 'reflect-metadata';
+import { plainToClass } from "class-transformer";
+import {Product} from "./product.model";
 
-console.log('Test')
+const products = [
+    { title: 'A Carpet', price: 29.99},
+    { title: 'A Book', price: 10.99}
+]
+
+const loadedProducts = plainToClass(Product, products);
+
+console.log(loadedProducts);
+
+const p1 = new Product('A Book', 12.99);
+
+console.log(p1.getInformation());
