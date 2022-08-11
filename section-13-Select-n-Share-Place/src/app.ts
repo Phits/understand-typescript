2abe0264a1f4f98f1d353d66f95275d7a520c0a1
John Fitzpatrick <john.fitzpatrick@sendflex.com>
2022-08-11 16:40:08 -0400

    Finished Select and Share Google Maps API

--- section-13-Select-n-Share-Place/src/app.ts
+++ section-13-Select-n-Share-Place/src/app.ts
@@ -1,1 +1,39 @@
-// Code goes here!
+const axios = require('axios').default;
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+const GOOGLE_API_KEY = '';
+
+type GoogleGeocodingResponse = {
+    results: { geometry: { location: { lat: number, lng: number } } }[];
+    status: 'OK' | 'ZERO_RESULTS';
+};
+
+function searchAddressHandler(event: Event){
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    // @ts-ignore
+    axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`
+    ).then((response: any) => {
+        if (response.data.status !== 'OK') {
+            throw new Error('Could not fetch location!')
+        }
+       const coordinates = response.data.results[0].geometry.location;
+        // @ts-ignore
+        const map = new google.maps.Map(document.getElementById("map"), {
+            center: coordinates,
+            zoom: 16,
+        });
+
+        new google.maps.Marker({
+            position: coordinates,
+            map: map,
+        });
+    }).catch((err: Error) => {
+        alert(err.message);
+        console.log(err);
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
